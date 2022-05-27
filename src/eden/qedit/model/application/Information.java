eeac6331a3c91acfcead9efea7add596e7c64868
BrendonIrwan <ed7-aspire4925@hotmail.com>
2022-05-27 03:40:19 +0000

    Development L

--- src/eden/qedit/model/application/Information.java
+++ src/eden/qedit/model/application/Information.java
@@ -14,13 +14,13 @@ public final class Information {
   public static final String NAME = "QEdit";
 
   /** Application version. */
-  public static final String VER = "devK";
+  public static final String VER = "devL";
 
   /** Application version, long version. */
-  public static final String VERSION = "Development K";
+  public static final String VERSION = "Development L";
 
   /** Application release date. */
-  public static final String DATE = "03/12/2022";
+  public static final String DATE = "05/26/2022";
 
   /** Application description. */
   public static final String DESCRIPTION
@@ -28,7 +28,7 @@ public final class Information {
 
   /** Application landing URL. */
   public static final String URL
-      = "https://ed7n.github.com/qedit";
+      = "https://ed7n.github.io/qedit";
 
   /** To prevent instantiations of this class. */
   private Information() {
